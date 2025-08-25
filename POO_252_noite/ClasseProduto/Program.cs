e3c454c12e077828f88de5eaf54e7caf4da664b5
AlecB <ale13black@gmail.com>
2025-08-25 19:52:20 -0300

    aula de 2508

--- POO_252_noite/ClasseProduto/Program.cs
+++ POO_252_noite/ClasseProduto/Program.cs
@@ -4,7 +4,7 @@ using ClasseProduto;
 internal class Program{
     private static void Main(string[] args)
     {
-        Produto p1;
+    /*    Produto p1;
 
         p1 = new Produto();
         p1.codigo = 1;
@@ -18,5 +18,24 @@ internal class Program{
         p1.MostrarAtributos();
         p1.CalcularSubtotal();
         p1.MostrarAtributos();
+    */
+        p2 = new Produto();
+        Console.WriteLine("Informe o nome do produto: ");
+        p2.nome = Console.ReadLine();
+        Console.WriteLine("Informe o estoque: ");
+        p2.estoque = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Informe o preço do produto: ");
+        p2.preco = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Informe o preço do produto: ");
+        p2.porcentagem = Convert.ToDouble(Console.ReadLine());
+        p2.MostrarAtributos();
+        p2.CalcularAumento();
+        p2.MostrarAtributos();
+        p2.RetirarEstoque();
+        p2.MostrarAtributos();
+        p2.CalcularSubtotal();
+        p2.MostrarAtributos();
+
+        
     }
 }
