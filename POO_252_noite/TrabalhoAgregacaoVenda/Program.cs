e9d70cd9d4d40f8992c9da16faa901e8d623f42d
Katakumi020 <ale13black@gmail.com>
2025-09-27 16:46:21 -0300

    maisummmmmmmm

---
+++ POO_252_noite/TrabalhoAgregacaoVenda/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TrabalhoAgregacaoVenda;
+
+internal class Program
+{
+    private static void Main()
+    {
+        Vendedor vendedor = new Vendedor("Robinston");
+        Comprador comprador = new Comprador("Alec", 2000);
+
+        Produto p1 = new Produto("PC Gaymer", 1500);
+        Produto p2 = new Produto("Bomba Para Treino", 120);
+        Produto p3 = new Produto("Jack Daniel's", 250);
+
+        Venda venda = new Venda(comprador, vendedor);
+
+        venda.RealizarVenda(p1);
+        venda.RealizarVenda(p2);
+        venda.RealizarVenda(p3);
+
+        venda.MostrarResumo();
+    }
+}
